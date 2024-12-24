0eb26b9ae01c32994b2e7f96552d3d67e9e6959e
jordan-furr <jordanfurr98@gmail.com>
2024-12-24 17:27:05 +0100

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="page flex-col ph5 borderbox text-center">
-        <p class="mb3">ezra is the most talented artist musican and the best brother!</p>
-        <p>as such a talented designer i think you would like to have more input on the structure and design of your site! im ready to start!</p>
+        <p class="mb3">ezra is the most talented artist & musican. the best brother!</p>
+        <p>as such a talented designer i thought you would like to have more input on the structure and design of your site. im ready to start!</p>
     </div>
 </template>
