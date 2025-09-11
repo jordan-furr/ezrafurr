57083a93ed312fd4856c3b9a7be179f68c92c8bd
jordan-furr <jordanfurr98@gmail.com>
2025-09-11 16:01:55 -0400

    preview image

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,23 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Ezra Furr",
-  description: "Artist, Producer, Engineer"
+  description: "Artist, Producer, Engineer",
+  openGraph: {
+    title: 'Ezra Furr',
+    description: 'Artist, Producer, Engineer',
+    url: 'https://www.ezrafurr.com/',
+    siteName: 'Ezra Furr',
+    images: [
+      {
+        url: 'https://www.ezrafurr.com/preview.png',
+        width: 1200,
+        height: 630,
+        alt: 'Ezra Furr',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  }
 };
 
 export default function RootLayout({
